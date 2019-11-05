814127e471e5a4ad1f310b6bb4f90f8d58ff20c1
Bold Lion <boldlion1988@gmail.com>
2019-11-05 15:33:18 +0200

    Initial Commit

---
+++ MyLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyLocation
+//
+//  Created by Bold Lion on 5.11.19.
+//  Copyright © 2019 Bold Lion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
